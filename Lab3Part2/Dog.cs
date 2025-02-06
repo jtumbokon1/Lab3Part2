827a2a51c56271412b0a6993a72e79c3eb934354
jtumbokon1 <jirchgot@gmail.com>
2025-02-06 11:31:08 -0700

    Created dog class

---
+++ Lab3Part2/Dog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Part2
+{
+    public class Dog : IAnimal
+    {
+        // public properties
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public int Age { get; set; }
+        public double Height { get; set; }
+
+        // constructor
+        public Dog(string name, string colour = "", int age = 0, double height = 0)
+        {
+            this.Name = name;
+            this.Colour = colour;
+            this.Age = age;
+            this.Height = height;
+        }
+
+        // public methods
+        public string Cry()
+        {
+            return $"Woof!";
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Dogs eat meat"); ;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\n" +
+                   $"Colour: {Colour}\n" +
+                   $"Age: {Age}\n" +
+                   $"Height: {Height}\"";
+        }
+    }// class
+}// namespace
